a90cb35c167505d58e2e5d3518ac555d3c3ee535
Md. Monir Hoshen <getmonirr@gmail.com>
2023-10-23 23:36:17 +0600

    some change in send notification

--- src/components/ScheduleTask.tsx
+++ src/components/ScheduleTask.tsx
@@ -23,19 +23,17 @@ const ScheduleTask = () => {
         dueReminders.forEach(async (reminder: Reminder) => {
           if (reminder.frequency === "daily") {
             // Schedule a daily reminder
-            const result = await axios.post(
+            axios.post(
               `${process.env.NEXT_PUBLIC_BASE_URL}/api/reminders/notify`,
               { reminderId: reminder._id, caregivers: reminder.caregivers }
             );
-            console.log({ daily: result });
           } else if (reminder.frequency === "weekly") {
             // Schedule weekly reminders on Sundays
             schedule.scheduleJob("0 0 * * 0", async () => {
-              const result = await axios.post(
+              axios.post(
                 `${process.env.NEXT_PUBLIC_BASE_URL}/api/reminders/notify`,
                 { reminderId: reminder._id, caregivers: reminder.caregivers }
               );
-              console.log({ weekly: result });
             });
           }
         });
@@ -49,31 +47,3 @@ const ScheduleTask = () => {
 };
 
 export default ScheduleTask;
-
-// const ScheduleTask = () => {
-//   const reminderSchedule = schedule.scheduleJob("* * * * *", async () => {
-//     const currentTime = moment(new Date()).format("h:mma").toUpperCase();
-//     try {
-//       const { data: dueReminders } = await axios.get(
-//         `${process.env.NEXT_PUBLIC_BASE_URL}/api/reminders?time=${currentTime}`
-//       );
-//       console.log(currentTime, dueReminders);
-
-//       if (dueReminders.length > 0) {
-//         // Trigger the notification route for each due reminder
-//         dueReminders.forEach((reminder: any) => {
-//           axios.post(
-//             `${process.env.NEXT_PUBLIC_BASE_URL}/api/reminders/notify`,
-//             { reminderId: reminder._id, caregivers: reminder.caregivers }
-//           );
-//         });
-//       }
-//     } catch (error) {
-//       console.error("Schedule Error :", error);
-//     }
-//   });
-
-//   return null;
-// };
-
-// export default ScheduleTask;
